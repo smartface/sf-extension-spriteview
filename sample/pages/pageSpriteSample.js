7213ca711c0f6d26b5e82d8b15a9dabbc1020e96
Eray Tuncer <eray.tuncer@smartface.io>
2017-04-10 10:50:33 +0300

    add sample.

---
+++ sample/pages/pageSpriteSample.js
@@ -0,0 +1,32 @@
+const extend    = require("js-base/core/extend");
+const Page      = require('nf-core/ui/page');
+const Image     = require('nf-core/ui/image');
+const ImageView = require('nf-core/ui/imageview');
+
+const SpriteView = require("sf-extension-spriteview");
+
+var SpritePage = new extend(Page)(
+    function(_super,params)
+    {
+        _super(this);
+
+        var characterSprite = new SpriteView({
+            width: 100, height: 125,
+            imageFillType: ImageView.FillType.ASPECTFIT
+        });
+        this.layout.addChild(characterSprite);
+        
+        characterSprite.setSprite({
+            sheet: Image.createFromFile("images://braid.png"),
+            frameX: 7,
+            frameY: 4,
+            frameCount: 27
+        });
+
+        this.onShow = function() {
+            characterSprite.play(1000);
+        };
+    }
+);
+
+module.exports = SpritePage;
